8de571afee5134bec7a6b1669e5593609364b870
al12us <rusalexis172@gmail.com>
2024-10-14 23:22:33 +0300

    using OpenTK(de completat)

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
 namespace RusuAlexandru_temaCLI
 {
     internal class Program
