9a7ef10673b670439d07d26938dfd675a2a60b6b
al12us <rusalexis172@gmail.com>
2024-10-17 19:14:43 +0300

    Am adaugat un triunghi folosind GL.Vertex2(x,y),GL.Color3(Color.Culoare),apoi am mai adaugat doua butoane de la tastatura

--- Program.cs
+++ Program.cs
@@ -14,14 +14,15 @@ namespace RusuAlexandru_temaCLI
 {
     class Program : GameWindow
     {
-        float angle;
+        float angle_Y;
         float mouse_positionY;
-
-        public Program() : base(800, 600)
+        float angle_X;
+        float mouse_positionX;
+        public Program() : base(800, 800)
         {
             VSync = VSyncMode.On;
             Console.WriteLine("OpenGl versiunea: " + GL.GetString(StringName.Version));
-            this.Title = "OpenGl versiunea: " + GL.GetString(StringName.Version) + " (mod imediat)";
+            this.Title = "OpenGl versiunea: " + GL.GetString(StringName.Version)+"-2024 CLI";
         }
         
 
@@ -29,7 +30,10 @@ namespace RusuAlexandru_temaCLI
         {
            // base.OnLoad(e);
             GL.ClearColor(Color.Chocolate);
+            mouse_positionX = Mouse.GetState().X;
             mouse_positionY = Mouse.GetState().Y;
+            
+           
         }
 
 
@@ -39,19 +43,27 @@ namespace RusuAlexandru_temaCLI
 
             GL.MatrixMode(MatrixMode.Projection);
             GL.LoadIdentity();
-            GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
+            GL.Ortho(-2.0, 2.0, -2.0, 2.0, 0.0, 4.0);
         }
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
             
             base.OnUpdateFrame(e);
             if (Keyboard.GetState().IsKeyDown(Key.W))
-                angle += 1f;
+                angle_Y += 1f;
             if (Keyboard.GetState().IsKeyDown(Key.S))
-                angle -= 1f;
+                angle_Y -= 1f;
+            if (Keyboard.GetState().IsKeyDown(Key.A))
+                angle_X -= 1f;
+            if (Keyboard.GetState().IsKeyDown(Key.D))
+                angle_X += 1f;
 
+            float mouseX = Mouse.GetState().X;
             float mouseY = Mouse.GetState().Y;
-             angle += (mouseY - mouse_positionY) * 0.1f;
+            
+             angle_X+=(mouseX-mouse_positionX)*0.1f;
+             angle_Y += (mouseY - mouse_positionY) * 0.1f;
+            mouse_positionX = mouseX;
             mouse_positionY = mouseY;
             if (Keyboard.GetState().IsKeyDown(Key.Escape))
                 Exit();
@@ -63,8 +75,8 @@ namespace RusuAlexandru_temaCLI
 
             GL.Clear(ClearBufferMask.ColorBufferBit);
             GL.PushMatrix();
-            GL.Rotate(angle, 0.0f, 0.0f, 1.0f);
-
+            GL.Rotate(angle_Y, 0.0f, 0.0f, 1.0f);
+            GL.Rotate(angle_X,0.0f,1.0f,0.0f);
             GL.Begin(PrimitiveType.LineStrip);
             GL.Color3(4.0f, 2.0f, 0.0f);
             GL.Vertex2(-0.5f, -0.5f);
@@ -75,9 +87,21 @@ namespace RusuAlexandru_temaCLI
             GL.Color3(1.0f, 0.0f, 3.0f);
             GL.Vertex2(0.9f, 0.4f);
             GL.End();
+            DrawObject();
 
             GL.PopMatrix();
             SwapBuffers();
+        }
+        void DrawObject()
+        {
+            GL.Begin(PrimitiveType.Triangles);
+            GL.Color3(Color.Yellow);
+            GL.Vertex2(1, 1);
+            GL.Color3(Color.Blue);
+            GL.Vertex2(1, 0);
+            GL.Color3(Color.Azure);
+            GL.Vertex2(2, 0);
+            GL.End();
         }
             [STAThread]
         static void Main(string[] args)
