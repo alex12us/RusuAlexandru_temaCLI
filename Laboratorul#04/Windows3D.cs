6510bde4262ab7d694c96402e8f3f79ef7b04ec7
al12us <rusalexis172@gmail.com>
2024-11-08 18:33:11 +0200

    Adaugarea claselor Windows3D,Grid,Axex,Randomizer,Camara3DIsometric

--- Laboratorul#04/Windows3D.cs
+++ Laboratorul#04/Windows3D.cs
@@ -1,18 +1,53 @@
 ﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTK.Input;
 
 namespace Laboratorul_04
 {
     class Windows3D:GameWindow
     {
-        public Windows3D():base(800,600,new OpenTK.Graphics.GraphicsMode(32,24,0,8))
+        public Windows3D():base(800,600,new GraphicsMode(32,24,0,8))
         {
 
             VSync = VSyncMode.On;
+        }
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            GL.ClearColor(Color.Blue);
+
+            //setare viewportului
+            GL.Viewport(0, 0, this.Width, this.Height);
+            //setare perspectiva
+            Matrix4 perspectiva = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4,(float)Width/(float)Height,1,256);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspectiva);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            SwapBuffers();
         }
     }
 }
