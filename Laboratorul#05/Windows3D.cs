f118ea294425aa7c7732256b7532e5d823a262ce
Alex <Alex@DESKTOP-V0CCMVM>
2024-11-24 20:52:20 +0200

    Cod OpenGL lab5

---
+++ Laboratorul#05/Windows3D.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+namespace Laboratorul_05
+{
+    class Windows3D:GameWindow
+    {
+        public Windows3D() : base(600, 800, new GraphicsMode(32, 24, 0, 8))
+        {
+            VSync = VSyncMode.On;
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            GL.ClearColor(Color.DarkViolet);
+
+            GL.Enable(EnableCap.DepthTest);
+            GL.DepthFunc(DepthFunction.Less);
+
+
+            GL.Hint(HintTarget.PolygonSmoothHint, HintMode.Nicest);
+
+        }
+        protected override void OnResize(EventArgs e) { base.OnResize(e); }
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+        }
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+        }
+
+    }
+}
