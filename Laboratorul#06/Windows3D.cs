6dbc7507d8b06d76defb2041beac15a801f47e26
Alex <Alex@DESKTOP-V0CCMVM>
2025-01-06 19:44:33 +0200

    Laboratoru 6 - adaugat,actualizat

---
+++ Laboratorul#06/Windows3D.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Graphics;
+
+namespace Laboratorul_06
+{
+   class Windows3D:GameWindow
+    {
+        public Windows3D():base(800, 600, new GraphicsMode(32, 24, 0, 8))
+        {
+
+        }
+    }
+}
